9c15c42fb228d143649878ccde36c48aa92bb6ad
ARBIYA SULTANA <115937326+sultanaarbiya@users.noreply.github.com>
2023-12-22 10:36:07 +0530

    script.js

--- script.js
+++ script.js
@@ -1,64 +1,29 @@
-let equalPressed = 0;
-let buttonInput = document.querySelectorAll(".button");
-let input = document.getElementById("input");
-let equal = document.getElementById("equal");
-let clear = document.getElementById("clear");
-let erase = document.getElementById("erase");
+let input = document.getElementById('inputbutton');
+let buttons = document.querySelectorAll('button');
 
-window.onload = () => {
-  input.value = "";
-};
-
-buttonInput.forEach((buttonClass) => {
-  buttonClass.addEventListener("click", () => {
-    if (equalPressed === 1) {
-      // input.value = "";
-      equalPressed = 0;
-    }
-    input.value += buttonClass.value;
-  });
-});
-
-equal.addEventListener("click", () => {
-  equalPressed = 1;
-  let inputValue = input.value;
-  try {
-    let solution = eval(inputValue);
-    if (Number.isNaN(solution) || !Number.isFinite(solution)) {
-      throw new Error("Invalid mathematical expression");
-    }
-    if (Number.isInteger(solution)) {
-      input.value = solution;
-    } else {
-      input.value = solution.toFixed(2);
-    }
-  } catch (error) {
-    alert(error.message);
-  }
-});
-equal.addEventListener("click", () => {
-  equalPressed = 1;
-  let inputValue = input.value;
-  console.log(inputValue, "hvyutfv");
-  try {
-    let solution = eval(inputValue);
-    if (Number.isNaN(solution) || !Number.isFinite(solution)) {
-      throw new Error("Invalid mathematical expression");
-    }
-    if (Number.isInteger(solution)) {
-      input.value = solution;
-    } else {
-      input.value = solution.toFixed(2);
-    }
-  } catch (error) {
-    alert(error.message);
-  }
-});
-
-clear.addEventListener("click", () => {
-  input.value = "";
-});
-
-erase.addEventListener("click", () => {
-  input.value = input.value.substr(0, input.value.length - 1);
-});
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(buttuon => {
+    buttuon.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == '=')
+        {
+            string = eval(string);
+            input.value = string;
+        }
+       else if(e.target.innerHTML == 'AC')
+        {
+            string = "";
+             input.value = string;
+         }
+       else if(e.target.innerHTML == 'DEL')
+         {
+              string = substring(0,string.length-1);
+               input.value = string;
+          }
+        else
+         {
+            string += e.target.innerHTML;
+            input.value = string;
+         }
+    })
+})
